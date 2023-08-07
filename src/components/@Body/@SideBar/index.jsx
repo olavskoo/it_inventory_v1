4babcdda66b39c19adf163093e8e35c54205ae39
olavskoo <63005403+olavskoo@users.noreply.github.com>
2023-08-07 17:31:46 -0600

    Font Awesome
    Adding font awesome

--- src/components/@Body/@SideBar/index.jsx
+++ src/components/@Body/@SideBar/index.jsx
@@ -6,7 +6,11 @@ export default function SideBar(){
 
     return(
         <>
-        <Styled.MainContainer></Styled.MainContainer>
+        <Styled.MainContainer>
+            <Styled.LinksContainer>Computer</Styled.LinksContainer>
+            <Styled.LinksContainer>Phones</Styled.LinksContainer>
+            <Styled.LinksContainer>Printers</Styled.LinksContainer>
+        </Styled.MainContainer>
         </>
     )
 }
--- src/components/@Body/@SideBar/styled.jsx
+++ src/components/@Body/@SideBar/styled.jsx
@@ -2,7 +2,16 @@ import styled from "styled-components"
 
 export const MainContainer = styled.div`
     display: flex;
+    flex-direction: column;
     width: 90%;
     height: 95%;
     background-color: #6400A0;
+`
+export const LinksContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 10%;
+    color: #E4E4E4;
 `
