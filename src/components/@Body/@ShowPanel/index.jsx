5fd6f5262c460af5501217f9e7a552ade819ab27
zerodegreespace <gustav0degree@gmail.com>
2023-08-09 11:18:57 -0600

    Body/tabla
    push nothing :(

--- src/components/@Body/@ShowPanel/index.jsx
+++ src/components/@Body/@ShowPanel/index.jsx
@@ -5,7 +5,8 @@ export default function ShowPanel (){
     
     return(
         <>
-        <Styled.MainContainer></Styled.MainContainer>
+        <Styled.MainContainer>
+        </Styled.MainContainer>
         </>
     )
 }
--- src/components/@Body/@ShowPanel/styled.jsx
+++ src/components/@Body/@ShowPanel/styled.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components"
 
 export const MainContainer = styled.div`
     display: flex;
+    flex-direction: column;
     width: 98%;
     height: 95%;
     border: 2px solid black;
