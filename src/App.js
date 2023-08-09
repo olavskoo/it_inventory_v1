efbbb9ae9a4be7610542fa8049613084aae07f13
olavskoo <63005403+olavskoo@users.noreply.github.com>
2023-08-09 12:18:44 -0600

    All
    Everything was destroyed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Home from "./components/@Home";
-import "./styles/global.css"
 
 function App() {
   return (
--- src/components/@Header/index.jsx
+++ src/components/@Header/index.jsx
@@ -1,21 +1,27 @@
 import React from "react";
-import * as Styled from "./styled"
-import GeLogoWhite from "../img/GeLogoWhite.png"
+import * as Styled from "./styled";
+import GeLogoWhite from "../img/GeLogoWhite.png";
 
-export default function Header (){
-    return(
-        <>
-        <Styled.MainContainer>
-            <Styled.LeftContainer>
-                <Styled.LeftContent>
-                    <a href="/"> <img src={GeLogoWhite} alt="GE Logo" style={{width:"100%", height:"100%"}}/></a>
-                </Styled.LeftContent>
-            </Styled.LeftContainer>
-            <Styled.RightContainer>
-                <Styled.RightContent></Styled.RightContent>
-            </Styled.RightContainer>
-        </Styled.MainContainer>   
-        </>
-    )
-    
+export default function Header() {
+  return (
+    <>
+      <Styled.MainContainer>
+        <Styled.LeftContainer>
+          <Styled.LeftContent>
+            <a href="/">
+              {" "}
+              <img
+                src={GeLogoWhite}
+                alt="GE Logo"
+                style={{ width: "100%", height: "100%" }}
+              />
+            </a>
+          </Styled.LeftContent>
+        </Styled.LeftContainer>
+        <Styled.RightContainer>
+          <Styled.RightContent></Styled.RightContent>
+        </Styled.RightContainer>
+      </Styled.MainContainer>
+    </>
+  );
 }
