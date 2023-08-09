560fda6c1cc1ccf1701dd6bf0f66abf1ac638cd3
olavskoo <63005403+olavskoo@users.noreply.github.com>
2023-08-09 14:51:53 -0600

    Roll back
    Volvemo al principio alv

--- src/components/@Body/@ShowPanel/index.jsx
+++ src/components/@Body/@ShowPanel/index.jsx
@@ -1,71 +1,11 @@
 import React from "react";
 import * as Styled from "./styled";
-import {
-  DataTable,
-  Table,
-  TableHead,
-  TableRow,
-  TableHeader,
-  TableBody,
-  TableCell,
-} from "@carbon/react";
 
-const rows = [
-  {
-    id: "a",
-    name: "Load balancer 1",
-    status: "Disabled",
-  },
-  {
-    id: "b",
-    name: "Load balancer 2",
-    status: "Starting",
-  },
-  {
-    id: "c",
-    name: "Load balancer 3",
-    status: "Active",
-  },
-];
-const headers = [
-  {
-    key: "name",
-    header: "Name",
-  },
-  {
-    key: "status",
-    header: "Status",
-  },
-];
 
 export default function ShowPanel() {
   return (
     <>
       <Styled.MainContainer>
-        <DataTable rows={rows} headers={headers}>
-          {({ rows, headers, getTableProps, getHeaderProps, getRowProps }) => (
-            <Table {...getTableProps()}>
-              <TableHead>
-                <TableRow>
-                  {headers.map((header) => (
-                    <TableHeader {...getHeaderProps({ header })}>
-                      {header.header}
-                    </TableHeader>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <TableRow {...getRowProps({ row })}>
-                    {row.cells.map((cell) => (
-                      <TableCell key={cell.id}>{cell.value}</TableCell>
-                    ))}
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          )}
-        </DataTable>
       </Styled.MainContainer>
     </>
   );
