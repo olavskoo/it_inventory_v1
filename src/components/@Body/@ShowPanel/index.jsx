3afced54aca139b115ff7eba5ee3efb766cd4c71
zerodegreespace <gustav0degree@gmail.com>
2023-08-10 10:11:07 -0600

    added table to show panel
    added table and search bar with carbon

--- src/components/@Body/@ShowPanel/index.jsx
+++ src/components/@Body/@ShowPanel/index.jsx
@@ -1,11 +1,75 @@
 import React from "react";
 import * as Styled from "./styled";
-
+import {
+  DataTable,
+  Table,
+  TableHead,
+  TableRow,
+  TableHeader,
+  TableBody,
+  TableCell,
+  TableToolbar,
+  TableToolbarSearch,
+} from "@carbon/react";
+const rows = [
+  {
+    id: "a",
+    name: "Load balancer 1",
+    status: "Disabled",
+  },
+  {
+    id: "b",
+    name: "Load balancer 2",
+    status: "Starting",
+  },
+  {
+    id: "c",
+    name: "Load balancer 3",
+    status: "Active",
+  },
+];
+const headers = [
+  {
+    key: "name",
+    header: "Name",
+  },
+  {
+    key: "status",
+    header: "Status",
+  },
+];
 
 export default function ShowPanel() {
   return (
     <>
       <Styled.MainContainer>
+        <DataTable rows={rows} headers={headers} isSortable >
+          {({ rows, headers, getTableProps, getHeaderProps, getRowProps, onInputChange}) => (
+            <Table {...getTableProps()}>
+              <TableHead>
+                <TableToolbar>
+                <TableToolbarSearch onChange={onInputChange}></TableToolbarSearch>
+                </TableToolbar>
+                <TableRow>
+                  {headers.map((header) => (
+                    <TableHeader {...getHeaderProps({ header })}>
+                      {header.header}
+                    </TableHeader>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow {...getRowProps({ row })}>
+                    {row.cells.map((cell) => (
+                      <TableCell key={cell.id}>{cell.value}</TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          )}
+        </DataTable>
       </Styled.MainContainer>
     </>
   );
