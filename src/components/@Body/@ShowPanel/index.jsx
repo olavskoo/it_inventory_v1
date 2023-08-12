ef7b69bf741b899e93f55c00b6d249d4e3ca4286
zerodegreespace <gustav0degree@gmail.com>
2023-08-12 11:48:31 -0600

    Added table with carbon
    search bar now working and table looks niceee

--- src/components/@Body/@ShowPanel/index.jsx
+++ src/components/@Body/@ShowPanel/index.jsx
@@ -1,12 +1,91 @@
 import React from "react";
 import * as Styled from "./styled";
-
+import {
+  DataTable,
+  Table,
+  TableHead,
+  TableRow,
+  TableHeader,
+  TableBody,
+  TableCell,
+  TableToolbar,
+  TableToolbarSearch,
+  Button,
+  TableToolbarContent,
+  Search,
+  TableContainer,
+  TableToolbarMenu,
+  TableToolbarAction
+} from "@carbon/react";
+const rows = [
+  {
+    id: "a",
+    name: "Load balancer 1",
+    status: "Disabled",
+  },
+  {
+    id: "b",
+    name: "Load balancer 2",
+    status: "Starting",
+  },
+  {
+    id: "c",
+    name: "Load balancer 3",
+    status: "Active",
+  },
+];
+const headers = [
+  {
+    key: "name",
+    header: "Name",
+  },
+  {
+    key: "status",
+    header: "Status",
+  },
+];
 
 export default function ShowPanel() {
   return (
     <>
       <Styled.MainContainer>
-        El Gustavo me la pela 
+      <DataTable
+  rows={rows}
+  headers={headers}
+  render={({ rows, headers, getHeaderProps, onInputChange }) => (
+    <TableContainer title="DataTable with toolbar">
+      <TableToolbar>
+        {/* pass in `onInputChange` change here to make filtering work */}
+        <TableToolbarSearch onChange={onInputChange} />
+        <TableToolbarContent>
+          <Button small kind="primary">
+            Add new
+          </Button>
+        </TableToolbarContent>
+      </TableToolbar>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {headers.map((header) => (
+              <TableHeader {...getHeaderProps({ header })}>
+                {header.header}
+              </TableHeader>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              {row.cells.map((cell) => (
+                <TableCell key={cell.id}>{cell.value}</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )}
+/>
       </Styled.MainContainer>
     </>
   );
