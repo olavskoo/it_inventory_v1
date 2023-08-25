7fcf08478b1c107ad7abc291226f5a703f091332
olavskoo <63005403+olavskoo@users.noreply.github.com>
2023-08-25 11:30:23 -0600

    header
    background colors

--- src/components/@Header/index.jsx
+++ src/components/@Header/index.jsx
@@ -1,30 +1,23 @@
 import React from "react";
 import * as Styled from "./styled";
-import GeLogoWhite from "../img/GE/GeLogoWhite.png";
+import GeLogoWhiteClean from "../img/GE/GeLogoWhiteClean.png";
 import BorderOpsColor from "../img/BorderOps/BorderOpsColor.png"
 
 export default function Header() {
   return (
     <>
       <Styled.MainContainer>
-        <Styled.LeftContainer>
-          <Styled.LeftContent>
-            <a href="/">
-              {" "}
-              <img
-                src={BorderOpsColor}
-                alt="GE Logo"
-                style={{ width: "100%", height: "100%" }}
-              />
-            </a>
-          </Styled.LeftContent>
-        </Styled.LeftContainer>
-        <Styled.RightContainer>
-          <Styled.RightContent>
-            <Styled.RightContentText>User</Styled.RightContentText>
-            <Styled.RightContentIcon><i class="fa-solid fa-user" style={{fontSize: "2em"}}></i></Styled.RightContentIcon>
-          </Styled.RightContent>
-        </Styled.RightContainer>
+          <Styled.LogoContainer>
+            <img src={GeLogoWhiteClean} alt="GELogoWhite" />
+          </Styled.LogoContainer>
+          <Styled.TitleContainer>
+            <h2>IT Inventory</h2>
+          </Styled.TitleContainer>
+          <Styled.UserContainer>
+            <button className="UserButton">
+               <i class="fa-regular fa-user" style={{fontSize: "2em"}}></i>
+            </button>
+          </Styled.UserContainer>
       </Styled.MainContainer>
     </>
   );
--- src/components/@Header/styled.jsx
+++ src/components/@Header/styled.jsx
@@ -2,53 +2,41 @@ import styled from "styled-components";
 
 export const MainContainer = styled.div`
   display: flex;
+  justify-content: space-around;
   width: 100%;
   height: 10%;
   background: rgb(100,0,160);
   background: linear-gradient(90deg, rgba(100,0,160,1) 0%, rgba(14,14,82,1) 100%);
 `;
 
-export const LeftContainer = styled.div`
+export const LogoContainer = styled.div`
+  display: flex;
+  width: 30%;
+  height: 100%;
+`
+
+export const TitleContainer =  styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 20%;
+  width: 30%;
   height: 100%;
-`;
-export const RightContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 80%;
-  height: 100%;
-`;
-export const LeftContent = styled.div`
-  display: flex;
-  width: 95%;
-  height: 93%;
-`;
-export const RightContent = styled.a`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  width: 98%;
-  height: 93%;
   color: #f4f4f8;
-  &:hover{
-    cursor: pointer;
-  }
-`;
-export const RightContentIcon = styled.div`
-  display: flex;
-  align-items: flex-end;
-  width: 3%;
-  height: 50%;
-`;
-export const RightContentText = styled.div`
+`
+
+export const UserContainer = styled.div`
   display: flex;
   justify-content: flex-end;
-  align-items: flex-end;
-  width: 5%;
-  height: 50%;
-  margin-right: .5em;
-`;
+  align-items: center;
+  width: 30%;
+  height: 100%;
+  & .UserButton{
+    border: none;
+    background: none;
+    color: #f4f4f8;
+    &:hover{
+      cursor: pointer;
+    }
+  }
+`
+
