9939276594967a41da317b387cc638e9f20f9ebc
zerodegreespace <gustav0degree@gmail.com>
2023-08-30 13:02:05 -0600

    Added forms for phones and printers
    added forms for printers and phones

--- src/components/@Body/@Forms/index.jsx
+++ src/components/@Body/@Forms/index.jsx
@@ -15,18 +15,18 @@ const siteItems=["BOP1","BOP2"]
       <Form>
         <Stack gap={7}>
         <TextInput
-            helperText="Optional helper text here; if message is more than one line text should wrap (~100 character count maximum)"
+            helperText="Only enter one name and one surname"
             id="Username"
             invalidText="Invalid error message."
             labelText="User"
-            placeholder="Name and surname"
+            placeholder="Name Surname"
           />
           <TextInput
-            helperText="Optional helper text here; if message is more than one line text should wrap (~100 character count maximum)"
+            helperText="Enter SSO"
             id="Sso"
             invalidText="Invalid error message."
             labelText="SSO"
-            placeholder="Enter SSO"
+            placeholder="Example: 999999999"
           />
           <Dropdown
           id="Department"
@@ -34,19 +34,21 @@ const siteItems=["BOP1","BOP2"]
           items={departmentItems}
           />
           <Dropdown
-          id="Department"
+          id="Brand"
           label="Select Brand"
           items={brandItems}
           />
           <TextInput
-            helperText="Optional helper text here; if message is more than one line text should wrap (~100 character count maximum)"
+            helperText="Enter the service tag of the PC"
             id="SerialNumber"
             invalidText="Invalid error message."
             labelText="Serial Number"
             placeholder="Enter Serial Number"
           />
            <Dropdown
+          
           id="Site"
+          labelText="Site"
           label="Select Site"
           items={siteItems}
           />
@@ -55,6 +57,125 @@ const siteItems=["BOP1","BOP2"]
   </>);
 }
 
+export function PhonesForm(){
+
+  const departmentItems=["IT","QA","Manufacture","Training","Production","Warehouse","Lean","Manteinance","HR","Finance"]
+const brandItems=["DELL","HP","Apple"]
+const siteItems=["BOP1","BOP2"]
+
+
+  return(<>
+      <Form>
+        <Stack gap={7}>
+        <TextInput
+            helperText="Only enter one name and one surname"
+            id="Username"
+            invalidText="Invalid error message."
+            labelText="User"
+            placeholder="Name Surname"
+          />
+          <TextInput
+            helperText="Enter SSO"
+            id="Sso"
+            invalidText="Invalid error message."
+            labelText="SSO"
+            placeholder="Example: 999999999"
+          />
+          <Dropdown
+          id="Department"
+          label="Select Department"
+          items={departmentItems}
+          />
+          <Dropdown
+          id="Brand"
+          label="Select Brand"
+          items={brandItems}
+          />
+          <TextInput
+            helperText="Enter the IMEI of the phone"
+            id="IMEI"
+            invalidText="Invalid error message."
+            labelText="IMEI"
+            placeholder="Enter IMEI"
+          />
+           <Dropdown
+          
+          id="Site"
+          label="Select Site"
+          items={siteItems}
+          />
+        </Stack>
+      </Form>
+  </>);
+
+}
+
+export function PrintersForm(){
+
+const brandItems=["ZEBRA","SATO",""]
+const siteItems=["BOP1","BOP2"]
+const areaItems=["Kilo","Case","Flex","V100"]
+
+  return(<>
+      <Form>
+        <Stack gap={7}>
+        <TextInput
+            helperText="Enter Printer Name"
+            id="Username"
+            invalidText="Invalid error message."
+            labelText="Printer name"
+            placeholder="Hostname"
+          />
+          <TextInput
+            helperText="Enter asset number"
+            id="Sso"
+            invalidText="Invalid error message."
+            labelText="Asset number"
+            placeholder="Z-xxx"
+          />
+          <TextInput
+            helperText="Enter Serial Number"
+            id="IMEI"
+            invalidText="Invalid error message."
+            labelText="Serial Number"
+            placeholder="Serial Number"
+          />
+          <Dropdown
+          id="Brand"
+          label="Select Brand"
+          items={brandItems}
+          />
+          <TextInput
+            helperText="Enter model of the printer"
+            id="Model"
+            invalidText="Invalid error message."
+            labelText="Printer Model"
+            placeholder="ZT-420"
+          />
+          <TextInput
+            helperText="Enter Printer IP"
+            id="IMEI"
+            invalidText="Invalid error message."
+            labelText="Printer IP"
+            placeholder="xxx.xxx.xxx.xxx"
+          />
+          <Dropdown
+          id="Area"
+          label="Select printer Area"
+          items={areaItems}
+          />
+          <Dropdown
+          helperText="Enter Site"
+          id="Site"
+          label="Select Site"
+          items={siteItems}
+          />
+
+        </Stack>
+      </Form>
+  </>);
+
+}
 
 export function Modal(){
   const button = useRef();
@@ -84,7 +205,7 @@ export function Modal(){
     }} launcherButtonRef={button}>
 <ModalHeader label="Account resources" title="Add a custom domain" />
 <ModalBody>
-<LaptopForm/>
+<PhonesForm/>
 </ModalBody>
 <ModalFooter primaryButtonText="Add" secondaryButtonText="Cancel" />
 </ComposedModal>}
